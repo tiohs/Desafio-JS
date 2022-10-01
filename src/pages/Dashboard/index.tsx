32c53b89f9885df26aa0af53c71928e71c48b160
tiohs <hamiltonsilva551@gmail.com>
2022-10-01 23:07:15 +0100

    transaction map

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -95,18 +95,14 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">Computer</td>
+                  <td className="income">R$ 5.000,00</td>
+                  <td>Sell</td>
+                  <td>20/04/2020</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
